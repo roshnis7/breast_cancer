56dee2b0203ec1de902d28200ad16c63f278d8d4
roshnis7 <47339436+roshnis7@users.noreply.github.com>
2019-02-08 13:06:03 -0500

    Update breast_cancer.R

--- breast_cancer.R
+++ breast_cancer.R
@@ -1,4 +1,3 @@
-setwd("C:/Users/roshn/Desktop/RBS/mav")
-wisc_bc_df <- read.csv("./wisc_bc_data.csv", stringsAsFactors = F)
-str(wisc_bc_df)
+
+wisc_bc_df <- read.csv("C:/Users/roshn/Desktop/RBS/mav/wisc_bc_df.csv")
 wisc_bc_df
